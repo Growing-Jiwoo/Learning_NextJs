d03afae4d72bfdb7c1b39b56a63ae2441f6c7433
Growing-Jiwoo <fbaa4412@naver.com>
2024-02-20 22:16:44 +0900

    섹션0, 1에 대한 학습 진행 결과 코드

---
+++ src/app/(beforeLogin)/@modal/(.)i/flow/signup/page.tsx
@@ -0,0 +1,10 @@
+import SignupModal from "@/app/(beforeLogin)/_component/SignUpModal";
+
+export default function Page() {
+  return (
+    <>
+      <span>interceptor</span>
+      <SignupModal />;
+    </>
+  );
+}
