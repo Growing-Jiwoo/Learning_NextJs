4c4f89d02859d296bdf610597aeae46eef3ad215
Growing-Jiwoo <fbaa4412@naver.com>
2024-02-21 17:06:57 +0900

    sessoin 2-1 : 오른쪽 섹션 =>  postForm, homeTab, post UI 생성

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,7 +1,34 @@
+import Post from "../_component/Post";
+import style from "./page.module.css";
+import PostForm from "./_component/PostForm";
+import Tab from "./_component/Tab";
+import TabProvider from "./_component/TabProvider";
+
 export default function Home() {
   return (
-    <main>
-      홈페이지
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
     </main>
-  )
+  );
 }
