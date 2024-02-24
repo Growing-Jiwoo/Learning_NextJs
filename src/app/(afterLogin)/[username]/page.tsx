63d92b4b80f05f25b33ac17021143385ebd3b9cc
Growing-Jiwoo <fbaa4412@naver.com>
2024-02-24 15:05:29 +0900

    section 3-2: 게시글 상세 페이지, 답글, 포토 모달 구현

--- src/app/(afterLogin)/[username]/page.tsx
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -1,18 +1,28 @@
-import style from './profile.module.css';
-import {dehydrate, HydrationBoundary, QueryClient} from "@tanstack/react-query";
-import UserPosts from "@/app/(afterLogin)/[username]/_component/UserPosts";
+import style from "./profile.module.css";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import UserInfo from "@/app/(afterLogin)/[username]/_component/UserInfo";
-import {getUser} from "@/app/(afterLogin)/[username]/_lib/getUser";
-import {getUserPosts} from "@/app/(afterLogin)/[username]/_lib/getUserPosts";
+import { getUser } from "@/app/(afterLogin)/[username]/_lib/getUser";
+import { getUserPosts } from "@/app/(afterLogin)/[username]/_lib/getUserPosts";
+import UserPosts from "./_component/UserPosts";
 
 type Props = {
-  params: { username: string },
-}
-export default async function Profile({params}: Props) {
-  const {username} = params;
+  params: { username: string };
+};
+export default async function Profile({ params }: Props) {
+  const { username } = params;
   const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({queryKey: ['users', username], queryFn: getUser})
-  await queryClient.prefetchQuery({queryKey: ['posts', 'users', username], queryFn: getUserPosts})
+  await queryClient.prefetchQuery({
+    queryKey: ["users", username],
+    queryFn: getUser,
+  });
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", "users", username],
+    queryFn: getUserPosts,
+  });
   const dehydratedState = dehydrate(queryClient);
 
   return (
@@ -24,5 +34,5 @@ export default async function Profile({params}: Props) {
         </div>
       </HydrationBoundary>
     </main>
-  )
+  );
 }
