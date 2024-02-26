ead95a85727794f3b77fc25ff2c82c223e5923ad
Growing-Jiwoo <fbaa4412@naver.com>
2024-02-26 20:49:39 +0900

    section4 : 서로 다른 컴포넌트간 query 일치하게 하기

--- Client/src/app/(afterLogin)/[username]/page.tsx
+++ Client/src/app/(afterLogin)/[username]/page.tsx
@@ -4,21 +4,19 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
+import UserPosts from "@/app/(afterLogin)/[username]/_component/UserPosts";
 import UserInfo from "@/app/(afterLogin)/[username]/_component/UserInfo";
-import { getUser } from "@/app/(afterLogin)/[username]/_lib/getUser";
 import { getUserPosts } from "@/app/(afterLogin)/[username]/_lib/getUserPosts";
-import UserPosts from "./_component/UserPosts";
+import { auth } from "@/auth";
+import { User } from "@/model/User";
 
 type Props = {
   params: { username: string };
 };
 export default async function Profile({ params }: Props) {
   const { username } = params;
+  const session = await auth();
   const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ["users", username],
-    queryFn: getUser,
-  });
   await queryClient.prefetchQuery({
     queryKey: ["posts", "users", username],
     queryFn: getUserPosts,
@@ -28,7 +26,7 @@ export default async function Profile({ params }: Props) {
   return (
     <main className={style.main}>
       <HydrationBoundary state={dehydratedState}>
-        <UserInfo username={username} />
+        <UserInfo username={username} session={session} />
         <div>
           <UserPosts username={username} />
         </div>
